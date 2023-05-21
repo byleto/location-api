5cfb7707abf7d37d846396deb933f0ba75ce93f1
Byron Torres <byleto@gmail.com>
2023-05-21 21:29:26 +0200

    feat: add dummy routes for location

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { locationRouter } from './routes/location.route';
 
 const app = express();
+
 app.use(json());
+app.use(locationRouter);
 
 app.listen(3001, () => {
     console.log('listening on http://localhost/30001');
