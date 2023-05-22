12d62d80d034881c08403ea6e34ec0ea76564d8a
Byron Torres <byleto@gmail.com>
2023-05-22 15:07:39 +0200

    feat: add cors configuration

--- src/controllers/locations.ts
+++ src/controllers/locations.ts
@@ -4,12 +4,14 @@ import { Locations } from "../models/locations";
 
 export const createLocation: RequestHandler = async (req, res, next) => {
   const locations = await Locations.create({ ...req.body });
+  res.set('Access-Control-Allow-Origin', '*');
   return res
     .status(200)
     .json({ message: "Location created successfully", data: locations });
 };
 
 export const deleteLocation: RequestHandler = async (req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
   const { id } = req.params;
   const deletedLocation: Locations | null = await Locations.findByPk(id);
   await Locations.destroy({ where: { id } });
@@ -19,6 +21,7 @@ export const deleteLocation: RequestHandler = async (req, res, next) => {
 };
 
 export const getAllLocations: RequestHandler = async (req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
   const allLocations: Locations[] = await Locations.findAll();
   return res
     .status(200)
