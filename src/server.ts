617dc0722d6ad23b5170710a04e2a564b3179bba
Byron Torres <byleto@gmail.com>
2023-05-23 11:31:32 +0200

    config: add cors configuration

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import { json } from 'body-parser';
 import router from './routes/locations';
 import connection from './config/db.config';
+const cors = require('cors');
 
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
 
 const app = express();
+
+app.use(cors({ origin: '*' }));
 
 app.use(json());
 app.use('/api', router);
